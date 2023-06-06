5dd3052b7bd76f4945a62dc4a715749e292de7f5
Bersheny <fhezrghjsd4@live.fr>
2023-06-06 10:22:36 +0200

    1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,6 +7,8 @@
 // Scripts
 // 
 
+
+// Navbar blanche au scroll
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -52,6 +54,8 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 });
 
+
+// Bouton pour revenir en haut
 window.addEventListener('load', function () {
     let btnTop = document.getElementById("btn-top");
     // display the button when user scrolls more than 20px
@@ -69,3 +73,51 @@ window.addEventListener('load', function () {
         document.documentElement.scrollTop = 0;
     });
 })
+
+// Horaires
+const time = () => {
+
+    const activerow = document.querySelector('#activerow');
+
+    const monday = document.querySelector('.monday');
+    const tuesday = document.querySelector('.tuesday');
+    const wednesday = document.querySelector('.wednesday');
+    const thursday = document.querySelector('.thursday');
+    const friday = document.querySelector('.friday');
+    const saturday = document.querySelector('.saturday');
+    const sunday = document.querySelector('.sunday');
+
+
+    switch (new Date().getDay()) {
+
+        case 1:
+            monday.setAttribute("id", "activerow");
+            break;
+        case 2:
+            tuesday.setAttribute("id", "activerow");
+            break;
+        case 3:
+            wednesday.setAttribute("id", "activerow");
+            break;
+        case 4:
+            thursday.setAttribute("id", "activerow");
+            break;
+        case 5:
+            friday.setAttribute("id", "activerow");
+            break;
+        case 6:
+            saturday.setAttribute("id", "activerow");
+            break;
+        case 0:
+            sunday.setAttribute("id", "activerow");
+            break;
+    }
+
+}
+time();
+
+// Enlever le # au top
+document.querySelector('a').addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo(0, 0);
+  });
