2658ee7c4139bdcc9abe6a2a22c48393f74fc0e7
Vincent Mazurais <fhezrghjsd4@live.fr>
2024-04-19 12:53:48 +0200

    Fix

--- migrations/Version20240415143305.php
+++ migrations/Version20240415143305.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240315094729 extends AbstractMigration
+final class Version20240415143305 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,7 +24,7 @@ final class Version20240315094729 extends AbstractMigration
         $this->addSql('CREATE TABLE cdb_partners (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, image VARCHAR(255) DEFAULT NULL, website_link VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_362449CAB03A8386 (created_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE cdb_recipes (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, time_days INT DEFAULT NULL, time_hours INT DEFAULT NULL, time_minutes INT NOT NULL, difficulty VARCHAR(255) NOT NULL, pricing VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_BA2B905CB03A8386 (created_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE cdb_recipes_featured (id INT AUTO_INCREMENT NOT NULL, featured INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE cdb_users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, role JSON NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_E416B294E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE cdb_users (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, role LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_E416B294E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE cdb_news ADD CONSTRAINT FK_8757A0C0B03A8386 FOREIGN KEY (created_by_id) REFERENCES cdb_users (id)');
         $this->addSql('ALTER TABLE cdb_partners ADD CONSTRAINT FK_362449CAB03A8386 FOREIGN KEY (created_by_id) REFERENCES cdb_users (id)');
         $this->addSql('ALTER TABLE cdb_recipes ADD CONSTRAINT FK_BA2B905CB03A8386 FOREIGN KEY (created_by_id) REFERENCES cdb_users (id)');
